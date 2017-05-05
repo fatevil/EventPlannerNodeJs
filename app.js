4aa9bc07702d8458a01485c9695c479883c61d28
fatevil <marekoka@seznam.cz>
2017-05-05 07:50:06 +0300

    Cleaning and finilization.

--- app.js
+++ app.js
@@ -5,49 +5,34 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const passport = require('passport');
 const upload = multer({
     dest: 'public/images/',
 });
-// [SH] Require Passport
-const passport = require('passport');
 
-// [SH] Bring in the data model
+
+// Model
 require('./app_api/models/db');
-// [SH] Bring in the Passport config after model is defined
+// Passport authentication strategy
 require('./app_api/config/passport');
-
-
-// [SH] Bring in the routes for the API (delete the default routes)
+// REST API routes
 const routesApi = require('./app_api/routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-//app.use(bodyParser.text());
-//app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// Set the app_client folder to serve static resources
-//app.use(express.static(path.join(__dirname, 'app_client')));
 
 // Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
+// Using the API routes
 app.use('/api', routesApi);
-
-/*
-app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
-});
-*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,7 +43,7 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// [SH] Catch unauthorised errors
+// Catch unauthorised errors
 app.use(function(err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         res.status(401);
@@ -67,7 +52,6 @@ app.use(function(err, req, res, next) {
 });
 
 // development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
